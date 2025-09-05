eed93ac0ae4f235b6167d4d819375651b2873758
Rodrigo <suportemusicdelivery1@gmail.com>
2025-09-05 15:17:36 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/bot-sync-watcher/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/store-analytics-dashboard/' : '/',
   server: {
     host: "::",
     port: 8080,
